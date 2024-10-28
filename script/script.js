a73184359d83f95587f2861897b21fb2aab1540b
eleonoracappellesso <cappellessoeleonora@gmail.com>
2024-10-28 15:38:17 +0100

    added if, else if and else and a final message

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-console.clear ();
+console.clear();
 
 /*
 
@@ -20,5 +20,26 @@ const junior = 17;
 const senior = 65;
 
 const totKm = parseInt(prompt("Quanti km vuoi percorrere?"));
+console.log(totKm);
 
-const passengerAge = parseInt(prompt ("Quanti anni ha il passeggero?"));
+
+const passengerAge = parseInt(prompt("Quanti anni ha il passeggero?"));
+console.log(passengerAge);
+
+const totPrice = totKm * priceKm;
+console.log(totPrice);
+
+let message;
+let finalPrice;
+
+if (passengerAge <= junior) {
+    finalPrice = totPrice - ((totPrice * juniorDiscount) / 100);
+    message = "E' stato applicato uno sconto del 20% riservato ai minorenni. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+} else if (passengerAge >= senior) {
+    finalPrice = totPrice - ((totPrice * seniorDiscount) / 100); 
+    message = "E' stato applicato uno sconto del 40% riservato agli over 65. Il costo finale del tuo biglietto è di €" + finalPrice.toFixed(2);
+} else {
+    message = "Il costo del tuo biglietto è di €" + totPrice.toFixed(2);
+}
+
+console.log(message);
